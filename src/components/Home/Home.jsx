894d544c52ccecd0970d25bb94d038a452a51a31
Masum <masumt999.com>
2023-04-07 05:08:25 +0600

    toast added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
+import {toast} from 'react-hot-toast';
 
 const Home = () => {
     const tshirts = useLoaderData()
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if(exists){
+             toast ('You Already Added This T-Shirt!!!') 
+        }
+        else{
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+        }
+        
+
+    const handleRemoveFromCart = id => {
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining)
+     }
+
+
     return (
-        <div>
-            <h1>Tshirts: {tshirts.length}</h1>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+            {
+                tshirts.map(tshirt => <TShirt
+                key={tshirt._id}
+                tshirt ={tshirt}
+                handleAddToCart ={handleAddToCart}
+                ></TShirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart 
+                cart={cart}
+                handleRemoveFromCart ={handleRemoveFromCart}
+                ></Cart>
+            </div>
         </div>
     );
 };
---
+++ src/components/TShirt/TShirt.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TShirt.css'
+
+const TShirt = ({tshirt, handleAddToCart}) => {
+
+    const {name, picture, price} = tshirt;
+    return (
+        <div className='t-shirt'>
+            <img src={picture}></img>
+            <h4>{name}</h4>
+            <p>Price: ${price}</p>
+            <button onClick={()=> handleAddToCart(tshirt)}>Buy Now</button>
+        </div>
+    );
+};
+
+export default TShirt;
